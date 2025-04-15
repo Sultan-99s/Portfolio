cb6e9087d304b520c167238a03c875ab106ec69c
bdcalling <sultan.shaon99@gmail.com>
2025-04-15 09:41:59 +0600

    updated CV

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
                   </div>
                   <div>
                     <a 
-                      href="/alex-chen-cv.pdf" 
+                      href="Public/Sultan_Moheuddin.pdf" 
                       download
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10 transition-colors"
                     >
